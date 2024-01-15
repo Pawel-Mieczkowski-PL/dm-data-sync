fa89c8d620ebc7b29027b060bedab0f6d044d3a2
Pawel Mieczkowski <peelen10@gmail.com>
2024-01-15 21:57:09 +0100

    test

--- functions/full-sync/full-sync.ts
+++ functions/full-sync/full-sync.ts
@@ -1,38 +1,39 @@
 import { Handler } from '@netlify/functions'
-// import { sanity, sanityAlgolia } from '../update/update'
+import { sanity, sanityAlgolia } from '../update/update'
 
 export const handler: Handler = async (event, context) => {
   {
 
     return {
       statusCode: 200,
-      body: 'ok',
+      body: 'xxx ' + process.env['ALGOLIA_APPLICATION_ID'],
     }
-    //   const _sanity = sanity // configured Sanity client
-    //   const _sanityAlgolia = sanityAlgolia // configured sanity-algolia
 
-    //   // Fetch the _id of all the documents we want to index
-    //   const types = ['article', 'seller', 'product']
-    //   const query = `* [_type in $types && !(_id in path("drafts.**"))][]._id`
+    const _sanity = sanity // configured Sanity client
+    const _sanityAlgolia = sanityAlgolia // configured sanity-algolia
 
-    //   return _sanity
-    //     .fetch(query, { types })
-    //     .then((ids) => {
-    //       ids = JSON.stringify(ids)
-    //       const body = `{"projectId":"v2n4gj8r","dataset":"production","ids":{"created":${ids},"deleted":[],"updated":[]}}`
-    //       _sanityAlgolia.webhookSync(_sanity, JSON.parse(body))
-    //     })
-    //     .then(() => {
-    //       return {
-    //         statusCode: 200,
-    //         body: 'ok',
-    //       }
-    //     })
-    //     .catch((err) => {
-    //       return {
-    //         statusCode: 500,
-    //         body: JSON.stringify({ message: err }),
-    //       }
-    //     })
+    // Fetch the _id of all the documents we want to index
+    const types = ['article', 'seller', 'product']
+    const query = `* [_type in $types && !(_id in path("drafts.**"))][]._id`
+
+    return _sanity
+      .fetch(query, { types })
+      .then((ids) => {
+        ids = JSON.stringify(ids)
+        const body = `{"projectId":"v2n4gj8r","dataset":"production","ids":{"created":${ids},"deleted":[],"updated":[]}}`
+        _sanityAlgolia.webhookSync(_sanity, JSON.parse(body))
+      })
+      .then(() => {
+        return {
+          statusCode: 200,
+          body: 'ok',
+        }
+      })
+      .catch((err) => {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({ message: err }),
+        }
+      })
   }
 }
